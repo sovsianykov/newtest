201079ff5a4924b29243c22840a7809e48fc392c
”sovsianykov” <swebproger@gmail.com>
2021-01-22 14:53:05 +0200

    for 5

---
+++ src/Main/Https/MyFetch.js
@@ -0,0 +1,8 @@
+import React from "react";
+import EasyHTTP from "./Https";
+
+const MyFetch =() => {
+    const https = new EasyHTTP();
+
+
+}
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Stage1 from "./Stages/Stage1";
 import '../App.css'
+import HTTP from "./Https/Https";
+import Button from '@material-ui/core/Button/Button'
+
 import {Container} from "@material-ui/core";
 
 let catStore = ['http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/cat-face.png'];
 
 function Main() {
+
 
     const [store,setStore] = React.useState(
         [
@@ -16,69 +20,23 @@ function Main() {
             {id: 5 , url: 'https://cdn140.picsart.com/321248359038211.png?type=webp&to=min&r=640'}
         ]
     )
+  const MyFetch =  () => {
 
+        HTTP.get('https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc')
+            // .then(response =>response)
+            .then(response =>console.log(response))
 
-    let j = 0;
-    const output = document.querySelector('#out')
-    const pic = document.querySelector('#picture')
-
-    async function myFetch() {
-        try {
-            let response = await fetch('https://api.thecatapi.com/v1/images/search?limit=3&page=10&order=Desc');
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            } else {
-                response = await response.json();
-                // console.log(response)
-
-                setStore(
-                    response.map(
-                      (  el,i) =>{
-                      return       store[i].url =el.url
-                                    catStore[i] = el.url
-                        }
-                    )
-                )
-                console.log(store)
-                console.log(catStore)
-
-
-            }
-
-        } catch (e) {
-            console.log(e);
-        }
-
-
+            .catch(err => console.log(err));
     }
-    j++
- const   showpic = () => {
 
-     // output.appendChild(catStore[j])
-     // console.log(output, j,catStore, catStore[j])
-     // output.removeChild(output.firstChild)
-     // if (catStore.length > 20) {
-     //     catStore.splice(0, 10)
-     //     j = catStore.length - 2
-
-     // }
-     }
-
-    const body = document.querySelector('body')
-
-
-    document.addEventListener('DOMContentLoaded', myFetch() )
-    body.addEventListener('click', (e) => {
-        // showpic();
-        myFetch()
-    })
 
 
 
     return (
 
             <div id='container'>
-          <Stage1  url={catStore[0]}/>
+          <Stage1  url={store[0].url}/>
+          <Button onClick={MyFetch}  >Press Me </Button>
           {/*<Stage1  url={store[0].url}/>*/}
             </div>
 
